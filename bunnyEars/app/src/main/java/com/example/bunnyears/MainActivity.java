801580a3ad5864f1f6c679c30abafeecb0e36acd
fgodoy3 <felipegodoy@gatech.edu>
2019-10-26 16:05:50 -0400

    speech to text & text to speech are functional

--- bunnyEars/app/src/main/java/com/example/bunnyears/MainActivity.java
+++ bunnyEars/app/src/main/java/com/example/bunnyears/MainActivity.java
@@ -63,4 +63,3 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
-
--- bunnyEars/app/src/main/java/com/example/bunnyears/SpeechActivity.java
+++ bunnyEars/app/src/main/java/com/example/bunnyears/SpeechActivity.java
@@ -1,33 +1,37 @@
 package com.example.bunnyears;
 
-import android.os.Build;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.text.format.DateUtils;
 import android.view.View;
-import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class SpeechActivity extends AppCompatActivity {
 
     private TextToSpeech myTTS;
     private SpeechRecognizer mySpeechRecognizer;
-
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+    private String sentence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -35,18 +39,35 @@ public class SpeechActivity extends AppCompatActivity {
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (ContextCompat.checkSelfPermission(SpeechActivity.this,
+                        Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    speak("You did not grant the app permission to use your microphone");
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(SpeechActivity.this,
+                            Manifest.permission.RECORD_AUDIO)) {
+                    } else {
+                        ActivityCompat.requestPermissions(SpeechActivity.this,
+                                new String[]{Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+                    }
+                } else {
+                    // Permission has been granted
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
+                    mySpeechRecognizer.startListening(intent);
+                }
             }
         });
 
+
         initializeTextToSpeech();
-        initiliazeSpeechRecognizer();
+        initializeSpeechRecognizer();
     }
 
-    private void initiliazeSpeechRecognizer() {
+    private void initializeSpeechRecognizer() {
         if (SpeechRecognizer.isRecognitionAvailable(this)) {
             mySpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
             mySpeechRecognizer.setRecognitionListener(new RecognitionListener() {
@@ -83,7 +104,8 @@ public class SpeechActivity extends AppCompatActivity {
                 @Override
                 public void onResults(Bundle bundle) {
                     List<String> results = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                    processResults(results.get(0));
+                    sentence = results.get(0);
+                    processResults(sentence);
                 }
 
                 @Override
@@ -96,20 +118,18 @@ public class SpeechActivity extends AppCompatActivity {
 
                 }
             });
+
         }
     }
 
-    private void processResults(String command) {
-        speak(command);
-        /*
-        command = command.toLowerCase();
-        if(command.contains("time")) {
-            Date now = new Date();
-            String time = DateUtils.formatDateTime(this, now.getTime(),
-                    DateUtils.FORMAT_SHOW_TIME);
-            speak("The time is " + time);
+    private void processResults(String sentence) {
+
+        speak("Here's what I understood: " + sentence);
+
+        if(sentence.contains("time")) {
+            String time = DateUtils.formatDateTime(this, new Date().getTime(),DateUtils.FORMAT_SHOW_TIME);
+            speak("Right now it's" + time);
         }
-        */
     }
 
     private void speak(String message) {
@@ -122,13 +142,19 @@ public class SpeechActivity extends AppCompatActivity {
         myTTS.shutdown();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializeSpeechRecognizer();
+        initializeTextToSpeech();
+    }
+
     private void initializeTextToSpeech() {
         myTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if(myTTS.getEngines().size() == 0) {
-//                    Toast.makeText(MainActivity.this, "this device doesn't support voice input",
-//                            Toast.LENGTH_LONG).show();
+                    speak("Your device does not support speech recognition");
                     finish();
                 } else {
                     myTTS.setLanguage(Locale.US);
