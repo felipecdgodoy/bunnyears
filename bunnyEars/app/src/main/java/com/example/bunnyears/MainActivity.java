9f75684c1c789b9e65ed481da36f3be8c48aa371
JoshMJ37 <jmjaffe37@gatech.edu>
2019-10-26 00:49:45 -0400

    Initial tab differentiation code added

--- bunnyEars/app/src/main/java/com/example/bunnyears/MainActivity.java
+++ bunnyEars/app/src/main/java/com/example/bunnyears/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This could be a record button", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
--- bunnyEars/app/src/main/java/com/example/bunnyears/ui/main/PageViewModel.java
+++ bunnyEars/app/src/main/java/com/example/bunnyears/ui/main/PageViewModel.java
@@ -12,7 +12,12 @@ public class PageViewModel extends ViewModel {
     private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
-            return "Hello world from section: " + input;
+            if (input == 1) {
+                return "Database Text here";
+            }
+            else {
+                return "Camera goes here";
+            }
         }
     });
 
