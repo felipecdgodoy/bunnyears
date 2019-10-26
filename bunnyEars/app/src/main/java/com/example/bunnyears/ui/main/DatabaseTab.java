1987d0e12c239b7ed82d6c94b288053ecd38ab0e
Peter Lebedev <plebedev3@gatech.edu>
2019-10-26 01:28:43 -0400

    Add tabs clases

---
+++ bunnyEars/app/src/main/java/com/example/bunnyears/ui/main/DatabaseTab.java
@@ -0,0 +1,4 @@
+package com.example.bunnyears;
+
+public class DatabaseTab {
+}
---
+++ bunnyEars/app/src/main/java/com/example/bunnyears/ui/main/PictureTab.java
@@ -0,0 +1,21 @@
+package com.example.bunnyears;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+public class PictureTab extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.picturetab,container, false);
+        return view;
+    }
+}
