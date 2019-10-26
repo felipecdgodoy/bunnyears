ee0285dfe6fcffa9854ec8e2d76d8d3276e34540
Amy Pollmann <apollmann3@gatech.edu>
2019-10-26 02:26:40 -0400

    Splash Screen

--- bunnyEars/app/src/main/java/com/example/bunnyears/MainActivity.java
+++ bunnyEars/app/src/main/java/com/example/bunnyears/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bunnyears;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,7 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
